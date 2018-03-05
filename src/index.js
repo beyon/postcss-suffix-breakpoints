517245223b44889e0e732ff7b328e166a1599953
beyon <bjorn@iamseriouslol.com>
2018-03-05 20:07:13 +0100

    Use breakpoints from options being passed in, initial options test.

--- src/index.js
+++ src/index.js
@@ -1,65 +1,97 @@
 // paste into: http://astexplorer.net/#/np0DfVT78g/94 for interactive testing
 import * as postcss from 'postcss';
 
-export default postcss.plugin('postcss-reverse-props', (options = {}) => {
-    // Work with options here
-    let breakpoints = [{ suffix: '-ns', var: '--breakpoint-not-small' },
-        { suffix: '-m', var: '--breakpoint-medium' },
-        { suffix: '-l', var: '--breakpoint-large' }
-    ];
-    let breakpointRules = new Map();
-    // init breakpoint_rules to hold empty arrays for all suffixes
-    for (let idx = 0; idx < breakpoints.length; idx++) {
-        breakpointRules.set( breakpoints[idx].suffix, [] );
-    }
-    return function (root, result) {
-        let classRules = [];
-        root.walkRules(rule => {
-            if ( rule.selector.startsWith('.') ) {
-                let isBreakpointRule = false;
-                for ( let bp = 0; bp < breakpoints.length; bp++) {
-                    let suffix = breakpoints[bp].suffix;
-                    if ( rule.selector.endsWith( suffix ) ) {
-                        isBreakpointRule = true;
-
-                        // update/add rule to rules for current suffix
-                        let suffixRules = breakpointRules.get( suffix );
-                        suffixRules.push(rule.clone());
-                        breakpointRules.set( suffix, suffixRules );
-
-                        // stop iterating, can only be one suffix per rule
-                        break;
-                    }
-                }
-                if ( isBreakpointRule ) rule.remove();
-                else classRules.push( rule.clone() );
+function filterInvalidBreakpoints(breakpoints, result) {
+    if (Array.isArray(breakpoints) && breakpoints.length > 0) {
+        breakpoints = breakpoints.filter((breakpoint, bpIdx) => {
+            let validSuffix = false;
+            let validAtMediaExpr = false;
+            if ('suffix' in breakpoint &&
+                typeof breakpoint.suffix === 'string' &&
+                breakpoint.suffix.length > 0) {
+                validSuffix = true;
+            } else {
+                result.warn('no valid suffix for breakpoint with index: ' +
+                            bpIdx.toString());
             }
+            if ('atMediaExpr' in breakpoint &&
+                typeof breakpoint.suffix === 'string' &&
+                breakpoint.atMediaExpr.length > 0) {
+                validAtMediaExpr = true;
+            } else {
+                result.warn('no valid atMediaExpr for breakpoint with index: ' +
+                            bpIdx.toString());
+            }
+            return validSuffix && validAtMediaExpr;
         });
+    } else {
+        // Nothing for plugin todo - warn?
+    }
+    // return { breakpoints, result };
+    return breakpoints;
+}
 
-        for (let bp = 0; bp < breakpoints.length; bp++)  {
-            let mediaClassRules = new Map();
-            let suffix = breakpoints[bp].suffix;
-            // copy class rules for all non suffixed rules and add suffix to
-            // selector for current breakpoint
-            for (let idx = 0; idx < classRules.length; idx++) {
-                const className = classRules[idx].selector;
-                const nameWithSuffix = className + suffix;
-                mediaClassRules.set(
-                    nameWithSuffix,
-                    classRules[idx].clone({ selector: nameWithSuffix }) );
+export default postcss.plugin(
+    'postcss-suffix-breakpoints',
+    ({ breakpoints = [] } = {} ) => {
+        return function (root, result) {
+            // ({ breakpoints, result } =
+            //     filterInvalidBreakpoints(breakpoints, result));
+            breakpoints = filterInvalidBreakpoints(breakpoints, result);
+            let breakpointRules = new Map();
+            // init breakpoint_rules to hold empty arrays for all suffixes
+            for (let idx = 0; idx < breakpoints.length; idx++) {
+                breakpointRules.set( breakpoints[idx].suffix, [] );
             }
-            // override suffixed rules with manually added ones
-            let manualSuffixRules = breakpointRules.get(suffix);
-            for ( let r = 0; r < manualSuffixRules.length; r++) {
-                mediaClassRules.set(
-                    manualSuffixRules[r].selector,
-                    manualSuffixRules[r].clone() );
-            }
-            root.append({
-                name: 'media',
-                params: '(' + breakpoints[bp].var + ')',
-                nodes: Array.from(mediaClassRules.values())
+            let classRules = [];
+            root.walkRules(rule => {
+                if ( rule.selector.startsWith('.') ) {
+                    let isBreakpointRule = false;
+                    for ( let bp = 0; bp < breakpoints.length; bp++) {
+                        let suffix = breakpoints[bp].suffix;
+                        if ( rule.selector.endsWith( suffix ) ) {
+                            isBreakpointRule = true;
+
+                            // update/add rule to rules for current suffix
+                            let suffixRules = breakpointRules.get( suffix );
+                            suffixRules.push(rule.clone());
+                            breakpointRules.set( suffix, suffixRules );
+
+                            // stop iterating, can only be one suffix per rule
+                            break;
+                        }
+                    }
+                    if ( isBreakpointRule ) rule.remove();
+                    else classRules.push( rule.clone() );
+                }
             });
-        }
-    };
-});
+
+            for (let bp = 0; bp < breakpoints.length; bp++)  {
+                let mediaClassRules = new Map();
+                let suffix = breakpoints[bp].suffix;
+                // copy class rules for all non suffixed rules and add suffix to
+                // selector for current breakpoint
+                for (let idx = 0; idx < classRules.length; idx++) {
+                    const className = classRules[idx].selector;
+                    const nameWithSuffix = className + suffix;
+                    mediaClassRules.set(
+                        nameWithSuffix,
+                        classRules[idx].clone({ selector: nameWithSuffix }) );
+                }
+                // override suffixed rules with manually added ones
+                let manualSuffixRules = breakpointRules.get(suffix);
+                for ( let r = 0; r < manualSuffixRules.length; r++) {
+                    mediaClassRules.set(
+                        manualSuffixRules[r].selector,
+                        manualSuffixRules[r].clone() );
+                }
+                root.append({
+                    name: 'media',
+                    params: '(' + breakpoints[bp].atMediaExpr + ')',
+                    nodes: Array.from(mediaClassRules.values())
+                });
+            }
+        };
+    });
+
+
--- tests/index.test.js
+++ tests/index.test.js
@@ -10,7 +10,26 @@ function run(input, output, opts) {
         });
 }
 
+function testOptionWarnings(warningCount, warnings, opts) {
+    return postcss([ plugin(opts) ]).process('body { }')
+        .then(result => {
+            let resultingWarnings = result.warnings();
+            expect(resultingWarnings.length).toBe(warningCount);
+            for (let w = 0; w < resultingWarnings.length; w++) {
+                let { text: warningText } = resultingWarnings[w];
+                expect(warningText).toEqual(warnings[w]);
+            }
+        });
+}
+
 it('sample test to get tests up and running', () => {
+    let options = {
+        breakpoints: [
+            { suffix: '-ns', atMediaExpr: '--breakpoint-not-small' },
+            { suffix: '-m', atMediaExpr: '--breakpoint-medium' },
+            { suffix: '-l', atMediaExpr: '--breakpoint-large' }
+        ]
+    };
     return run(
         // input:
         `
@@ -26,6 +45,18 @@ it('sample test to get tests up and running', () => {
 @media (--breakpoint-large) {
 .flex-l { display: flex; } }
         `,
-        // options:
-        { });
+        options);
+});
+
+it('Options suffix warning', () => {
+    let options = {
+        breakpoints: [
+            { suffix: '-ns', atMediaExpr: '--SomeVar' },
+            { suffix: '-m', atMediaExpr: '' }
+        ]
+    };
+    return testOptionWarnings(
+        1,
+        ['no valid atMediaExpr for breakpoint with index: 1'],
+        options);
 });
