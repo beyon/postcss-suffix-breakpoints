aae4dd9d940ba563122b95ece7317a7ed2b17f19
beyon <bjorn@iamseriouslol.com>
2018-03-05 20:09:21 +0100

    built transpiled index.js. Should it be tracked though?

--- index.js
+++ index.js
@@ -10,21 +10,43 @@ var postcss = _interopRequireWildcard(_postcss);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-// var postcss = require('postcss');
+function filterInvalidBreakpoints(breakpoints, result) {
+    if (Array.isArray(breakpoints) && breakpoints.length > 0) {
+        breakpoints = breakpoints.filter(function (breakpoint, bpIdx) {
+            var validSuffix = false;
+            var validAtMediaExpr = false;
+            if ('suffix' in breakpoint && typeof breakpoint.suffix === 'string' && breakpoint.suffix.length > 0) {
+                validSuffix = true;
+            } else {
+                result.warn('no valid suffix for breakpoint with index: ' + bpIdx.toString());
+            }
+            if ('atMediaExpr' in breakpoint && typeof breakpoint.suffix === 'string' && breakpoint.atMediaExpr.length > 0) {
+                validAtMediaExpr = true;
+            } else {
+                result.warn('no valid atMediaExpr for breakpoint with index: ' + bpIdx.toString());
+            }
+            return validSuffix && validAtMediaExpr;
+        });
+    } else {}
+    // Nothing for plugin todo - warn?
 
-// module.exports = postcss.plugin('postcss-suffix-breakpoints', function (opts) {
-exports.default = postcss.plugin('postcss-reverse-props', function () {
-    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    // return { breakpoints, result };
+    return breakpoints;
+} // paste into: http://astexplorer.net/#/np0DfVT78g/94 for interactive testing
+exports.default = postcss.plugin('postcss-suffix-breakpoints', function () {
+    var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+        _ref$breakpoints = _ref.breakpoints,
+        breakpoints = _ref$breakpoints === undefined ? [] : _ref$breakpoints;
 
-    // opts = opts || {};
-    // Work with options here
-    var breakpoints = [{ suffix: '-ns', var: '--breakpoint-not-small' }, { suffix: '-m', var: '--breakpoint-medium' }, { suffix: '-l', var: '--breakpoint-large' }];
-    var breakpointRules = new Map();
-    // init breakpoint_rules to hold empty arrays for all suffixes
-    for (var idxInit = 0; idxInit < breakpoints.length; idxInit++) {
-        breakpointRules.set(breakpoints[idxInit].suffix, []);
-    }
     return function (root, result) {
+        // ({ breakpoints, result } =
+        //     filterInvalidBreakpoints(breakpoints, result));
+        breakpoints = filterInvalidBreakpoints(breakpoints, result);
+        var breakpointRules = new Map();
+        // init breakpoint_rules to hold empty arrays for all suffixes
+        for (var idx = 0; idx < breakpoints.length; idx++) {
+            breakpointRules.set(breakpoints[idx].suffix, []);
+        }
         var classRules = [];
         root.walkRules(function (rule) {
             if (rule.selector.startsWith('.')) {
@@ -52,10 +74,10 @@ exports.default = postcss.plugin('postcss-reverse-props', function () {
             var suffix = breakpoints[bp].suffix;
             // copy class rules for all non suffixed rules and add suffix to
             // selector for current breakpoint
-            for (var idx = 0; idx < classRules.length; idx++) {
-                var className = classRules[idx].selector;
+            for (var _idx = 0; _idx < classRules.length; _idx++) {
+                var className = classRules[_idx].selector;
                 var nameWithSuffix = className + suffix;
-                mediaClassRules.set(nameWithSuffix, classRules[idx].clone({ selector: nameWithSuffix }));
+                mediaClassRules.set(nameWithSuffix, classRules[_idx].clone({ selector: nameWithSuffix }));
             }
             // override suffixed rules with manually added ones
             var manualSuffixRules = breakpointRules.get(suffix);
@@ -64,9 +86,9 @@ exports.default = postcss.plugin('postcss-reverse-props', function () {
             }
             root.append({
                 name: 'media',
-                params: '(' + breakpoints[bp].var + ')',
+                params: '(' + breakpoints[bp].atMediaExpr + ')',
                 nodes: Array.from(mediaClassRules.values())
             });
         }
     };
-}); // paste into: http://astexplorer.net/#/np0DfVT78g/94 for interactive testing
+});
