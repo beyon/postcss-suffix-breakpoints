f62fcf518c7d0cd4b56042673a931c2b849cd366
beyon <bjorn@iamseriouslol.com>
2018-03-08 15:16:20 +0100

    classSelectorRegex in new scratchpad folder
    Use scratchpad folder for possible usable code snippets etc
    (that doesn't belong in the src folder)

---
+++ scratchpad/cssClassSelectorRegex.js
@@ -0,0 +1,5 @@
+// Might come in handy, considered using with
+// root.walk(classSelectorRegex, ruleNode => {} )
+// but wasn't a very good idea in that place
+const classSelectorRegex =
+    /^\.-?[_a-zA-Z]+[_a-zA-Z0-9-]*(:[_a-zA-Z0-9-]+)*/;
