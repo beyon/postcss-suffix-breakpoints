98a0ea8ba3e39a8edda51b2b238061fb80f3d524
beyon <bjorn@iamseriouslol.com>
2018-03-09 14:28:38 +0100

    Updated tests to match new formatting code
    Removed chopPseudo.test.js file, not sure how to test non-exported
    function.
    
    Used parts of chopPseudo covered by other tests.
    Consider simplifying chopPseudo since the pseudo and pSeparator
    properties are never used.

--- tests/index.test.js
+++ tests/index.test.js
@@ -39,12 +39,18 @@ it('Basic breakpoint generation', () => {
         // output:
         `
 .flex { display: flex; }
+
 @media (--breakpoint-not-small) {
-.flex-ns { display: flex; } }
+    .flex-ns { display: flex; }
+}
+
 @media (--breakpoint-medium) {
-.flex-m { display: flex; } }
+    .flex-m { display: flex; }
+}
+
 @media (--breakpoint-large) {
-.flex-l { display: flex; } }
+    .flex-l { display: flex; }
+}
         `,
         tachyonsBreakpoints);
 });
@@ -62,12 +68,106 @@ p { text-color: pink; }
 body { text-color: black; }
 .flex { display: flex; }
 p { text-color: pink; }
+
 @media (--breakpoint-not-small) {
-.flex-ns { display: flex; } }
+    .flex-ns { display: flex; }
+}
+
 @media (--breakpoint-medium) {
-.flex-m { display: flex; } }
+    .flex-m { display: flex; }
+}
+
 @media (--breakpoint-large) {
-.flex-l { display: flex; } }
+    .flex-l { display: flex; }
+}
+        `,
+        tachyonsBreakpoints);
+});
+
+it('Mixed test', () => {
+    return run(
+        // input:
+        `
+body { text-color: black; }
+
+.flex { display: flex; }
+.red {
+    text-color: red;
+    bg-color: pink;
+}
+
+.green { text-color: green; }
+
+p { text-color: pink; } /* comment */
+
+.flex-m { display: inline-flex; } /* manual */
+
+/* !no-suffix */
+.fish:hover { text-color: blue; }
+
+.pseudo { text-color: black; }
+.pseudo:hover { text-color: gold; }
+.pseudo-ns:hover { text-color: silver; }
+.pseudoelement::after { content: " - This comes after"; }
+
+        `,
+        // output:
+        `
+body { text-color: black; }
+
+.flex { display: flex; }
+.red {
+    text-color: red;
+    bg-color: pink;
+}
+
+.green { text-color: green; }
+
+p { text-color: pink; } /* comment */
+
+/* !no-suffix */
+.fish:hover { text-color: blue; }
+
+.pseudo { text-color: black; }
+.pseudo:hover { text-color: gold; }
+.pseudoelement::after { content: " - This comes after"; }
+
+@media (--breakpoint-not-small) {
+    .flex-ns { display: flex; }
+    .red-ns {
+        text-color: red;
+        bg-color: pink;
+    }
+    .green-ns { text-color: green; }
+    .pseudo-ns { text-color: black; }
+    .pseudo-ns:hover { text-color: silver; }
+    .pseudoelement-ns::after { content: " - This comes after"; }
+}
+
+@media (--breakpoint-medium) {
+    .flex-m { display: inline-flex; } /* manual */
+    .red-m {
+        text-color: red;
+        bg-color: pink;
+    }
+    .green-m { text-color: green; }
+    .pseudo-m { text-color: black; }
+    .pseudo-m:hover { text-color: gold; }
+    .pseudoelement-m::after { content: " - This comes after"; }
+}
+
+@media (--breakpoint-large) {
+    .flex-l { display: flex; }
+    .red-l {
+        text-color: red;
+        bg-color: pink;
+    }
+    .green-l { text-color: green; }
+    .pseudo-l { text-color: black; }
+    .pseudo-l:hover { text-color: gold; }
+    .pseudoelement-l::after { content: " - This comes after"; }
+}
+
         `,
         tachyonsBreakpoints);
 });
