f28bf9e2ff26db127d92dade200d158e0abe6b41
beyon <bjorn@iamseriouslol.com>
2018-04-03 11:47:30 +0200

    Tests for no options, zero-length breakpoints[], non array breakpoints[]

--- tests/breakpoint-validation.test.js
+++ tests/breakpoint-validation.test.js
@@ -16,6 +16,26 @@ function testOptionWarnings(warningCount, warnings, opts) {
         });
 }
 
+function testNoOptions() {
+    return postcss([ plugin() ]).process('body { }')
+        .then(result => {
+            let resultingWarnings = result.warnings();
+            const warnings = [
+                'options.breakpoints[] has zero elements',
+                'Nothing to do! (No valid breakpoints supplied)'
+            ];
+            expect(resultingWarnings.length).toBe(2);
+            for (let w = 0; w < resultingWarnings.length; w++) {
+                let { text: warningText } = resultingWarnings[w];
+                expect(warningText).toEqual(warnings[w]);
+            }
+        });
+}
+
+it('has no options', () => {
+    return testNoOptions();
+});
+
 it('No valid breakpoints', () => {
     let options = {
         breakpoints: [
@@ -28,6 +48,32 @@ it('No valid breakpoints', () => {
         [
             'no valid suffix for breakpoint with index: 0',
             'no valid atMediaExpr for breakpoint with index: 1',
+            'Nothing to do! (No valid breakpoints supplied)'
+        ],
+        options);
+});
+
+it('is not an array', () => {
+    let options = {
+        breakpoints: null
+    };
+    return testOptionWarnings(
+        2,
+        [
+            'options.breakpoints is not an array',
+            'Nothing to do! (No valid breakpoints supplied)'
+        ],
+        options);
+});
+
+it('is zero element array', () => {
+    let options = {
+        breakpoints: []
+    };
+    return testOptionWarnings(
+        2,
+        [
+            'options.breakpoints[] has zero elements',
             'Nothing to do! (No valid breakpoints supplied)'
         ],
         options);
